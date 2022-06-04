073674ac44c382fdc104c958c3762de74bcbc3aa
anOrgandroiD <anorgandroid@outlook.com>
2022-06-04 18:50:42 -0400

    added first 100 skills + various other fixes

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ import {Compendium} from "./data/compendium.js"
 import {Party} from "./data/party.js"
 import {Player} from "./data/player.js"
 
-let player = new Player("Toyota Carolla", [5, 0, 12, 0, 0, 0, 0, 0], [0, 1])
+let player = new Player("Toyota Carolla", [5, 0, 12, 0, 0, 0, 0, 0], [109])
 let compendium = new Compendium()
 let party = new Party(player)
-party.pushNewActor(compendium.getDemon(0), [5, 0, 15, 0, 0, 0, 0, 0], [0, 1])
+party.pushNewActor(compendium.getDemon(0), [5, 0, 15, 0, 0, 0, 0, 0], [0])
+party.pushNewActor(compendium.getDemon(1), [5, 0, 15, 0, 0, 0, 0, 0], [125])
 party.addItem(compendium.getItem(0), 5)
 party.displayParty()
