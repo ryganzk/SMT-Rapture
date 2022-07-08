ebb6b90260a580da499462f977c0464ba92a6f0d
anOrgandroiD <anorgandroid@outlook.com>
2022-07-08 08:41:52 -0400

    updated player and party structures

--- data/party.js
+++ data/party.js
@@ -19,11 +19,33 @@ export class Party {
       * 
       * @method
       * @param {Player} player
+      * @param {Ruleset} rules
       *****************************************************************************/
-    constructor(player) {
+    constructor(player, rules) {
         this.player = Object.values(player)[0]
+        this.rules = rules
         this.actors = []
+        this.active = []
         this.items = []
+
+        // Makes sure the player's level isn't above the ruleset's limit
+        if (this.player.level > this.rules.levelLimit) {
+            throw `${this.player.name}'s level is higher than the limit of ${this.rules.levelLimit}`
+        }
+
+        if(this.player.skills.length > rules.moveLimit) {
+            throw `${this.player.name} cannot know more than ${rules.moveLimit} moves`
+        }
+
+        for (let i = 0; i < this.player.skills.length; ++i) {
+            if (this.player.skills[i].unique && this.player.skills[i].unique != 999 && this.rules.standardRules) {
+                throw `${this.player.name} cannot use the unique move ${this.player.skills[i].name}`
+            }
+
+            if (this.player.skills[i].level > this.rules.highestRank) {
+                throw `${this.player.name} cannot learn ${this.player.skills[i].name}, as it's too powerful for the current ruleset`
+            }
+        }
     }
 
     /*****************************************************************************
@@ -37,45 +59,137 @@ export class Party {
       * @throws If the player wants to add more baseStats than the demon's level allows
       *****************************************************************************/
     pushNewActor(demon, statMods, skills) {
-        try {
-            if (statMods[1] + statMods[2] > (statMods[0] - demon.level) * 5
-            || statMods[3] + statMods[4] + statMods[5] + statMods[6] + statMods[7]
-            > (statMods[0] - demon.level) * 3)
-                throw `Too many stats have been pumped into ${demon.name}!`
+        // Makes sure the demon's level isn't lower than the minimum possible level for that demon
+        if (statMods[0] < demon.level) {
+            throw `${demon.name}'s level is too low! Must be ${demon.level} or higher!`
+        }
 
-            demon.level = statMods[0]
-            demon.baseStats.hp += statMods[1]
-            demon.baseStats.mp += statMods[2]
-            demon.baseStats.strength += statMods[3]
-            demon.baseStats.vitality += statMods[4]
-            demon.baseStats.magic += statMods[5]
-            demon.baseStats.agility += statMods[6]
-            demon.baseStats.luck += statMods[7]
+        // Makes sure the player's level isn't above the ruleset's limit
+        if (statMods[0] > this.rules.levelLimit) {
+            throw `${demon.name}'s level is higher than the limit of ${this.rules.levelLimit}!`
+        }
 
-            let battleStats = {
-                battleStats: {
-                    hp: demon.baseStats.hp,
-                    mp: demon.baseStats.mp
+        if(skills.length > this.rules.moveLimit) {
+            throw `${demon.name} cannot know more than ${this.rules.moveLimit} moves`
+        }
+
+        if (statMods[1] + statMods[2] > (statMods[0] - demon.level) * 5
+        || statMods[3] + statMods[4] + statMods[5] + statMods[6] + statMods[7]
+        > (statMods[0] - demon.level) * 3) {
+            throw `Too many stats have been pumped into ${demon.name}!`
+        }
+
+        demon.level = statMods[0]
+        demon.baseStats.hp += statMods[1]
+        demon.baseStats.mp += statMods[2]
+        demon.baseStats.strength += statMods[3]
+        demon.baseStats.vitality += statMods[4]
+        demon.baseStats.magic += statMods[5]
+        demon.baseStats.agility += statMods[6]
+        demon.baseStats.luck += statMods[7]
+
+        let base = false
+
+        // If the demon doesn't include any moves, assume base skills
+        if(skills.length == 0) {
+            base = true
+            skills = demon.baseSkills
+            
+            // Depending on the demon's level, give appropriate level-up skills
+            for (let i = 0; i < demon.levelSkills.length; ++i) {
+                if (statMods[0] >= demon.levelSkills[i][1]) {
+                    skills.push(demon.levelSkills[i][0])
+                }
+                else {
+                    break
+                }
+            }
+        }
+        
+        for(let i = 0; i < skills.length; i++) {
+            skills[i] = skillCompendium.getSkill(skills[i])
+
+            if (skills[i].unique && skills[i].unique != demon.demID) {
+                throw `${demon.name} cannot use the unique move ${skills[i].name}`
+            }
+
+            if (skills[i].level > this.rules.highestRank && !base) {
+                throw `${demon.name} cannot learn ${skills[i].name}, as it's too powerful for the current ruleset`
+            }
+
+            determinePotentials(demon.potentials, skills[i])
+        }
+
+        // Makes sure the demon doesn't include duplicate skills
+        let dupCheck = new Set(skills)
+            if (skills.length !== dupCheck.size) {
+                for (let i = 0; i < skills.length; ++i) {
+                    if (!dupCheck.has(skills[i])) {
+                        throw `${demon.name} knows ${skills[i].name} more than once!`
+                    }
+                    else {
+                        dupCheck.delete(skills[i])
+                    }
                 }
             }
 
-            Object.assign(demon, battleStats)
-
-            if(skills.length != 0) 
-                demon.skills = skills
-            
-            for(let i = 0; i < demon.skills.length; i++) {
-                demon.skills[i] = skillCompendium.getSkill(demon.skills[i])
-                determinePotentials(demon.potentials, demon.skills[i])
-            }
-            
-            demon.boosts = [0, 0, 0, 0]
-            demon.guard = 0
-
-            this.actors.push(demon)
+        let addendum = {
+            battleStats: {
+                hp: demon.baseStats.hp,
+                mp: demon.baseStats.mp
+            },
+            skills: skills
         }
-        catch (err) {
-            console.log(err)
+
+        Object.assign(demon, addendum)
+        
+        /*************************************************************************
+          * 0: AILMENT
+          * 1: CHARGE
+          * 2: PROTECTIVE
+          * 3: DAMAGE DOWN
+          * 4: TAUNT
+          * 5: ATTACK
+          * 6: DEFENSE
+          * 7: ACCURACY/EVASION
+          * 
+          * ARRAYS WITHIN THE ARRAY REPRESENTS: [BOOST, TURNS LEFT]
+          *************************************************************************/
+
+        demon.boosts = [[0, 0, 0], 0, 0, 0, [0, 0], [0, 0], [0, 0], [0, 0]]
+
+        /*************************************************************************
+          * 0: HP AID
+          * 1: MP AID
+          * 2: PHYS DAMAGE
+          * 3: FIRE DAMAGE
+          * 4: ICE DAMAGE
+          * 5: ELEC DAMAGE
+          * 6: FORCE DAMAGE
+          * 7: LIGHT DAMAGE
+          * 8: DARK DAMAGE
+          * 10: ALMIGHTY DAMAGE
+          * 11: MASK
+          * 12: RESTORE
+          * 13: CURSE SIPHON
+          * 14: COUNTER
+          * 15: EYE
+          * 16: GLEE
+          * 17: POISON
+          * 18: BOON BOOST
+          * 19: ENDURE
+          * 20: SAFEGUARD
+          * 21: CRITICAL ZEALOT
+          * 22: HEAVENLY COUNTER
+          *************************************************************************/
+
+        demon.passives = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        demon.guard = 0
+
+        this.actors.push(demon)
+
+        if (this.active.length < 3) {
+            this.active.push(demon)
         }
     }
 
